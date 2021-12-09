5f2667b1cc241360b26c49ff7f652ecf28e2090c
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2021-12-09 08:37:20 +0100

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+    int stranicaA=6;
+int stranicaB=8;
+int stranicaC=4;
+    printf("Unesi stranicu\n");
+    scanf("%d %d %d",&stranicaA, &stranicaB, &stranicaC);
+
+int mnozenje=stranicaA*stranicaB*stranicaC;
+printf("%d",mnozenje);
+}
