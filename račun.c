3fbebca3ea7f80ab56629f7bfa4323e80832246e
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-01-13 00:39:07 +0100

    Add files via upload

---
+++ račun.c
@@ -0,0 +1,20 @@
+#include <stdio.hr>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a;
+    int b;
+    int c;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("površina trokuta je = %d\n", ((a*b*c));
+    return 0;
+
+
+
+
+}
