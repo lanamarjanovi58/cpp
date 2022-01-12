ba7eb465ef29adee5dfb43cf31ef9e381e744050
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-01-13 00:16:50 +0100

    Add files via upload

---
+++ Relacijski operatori.c
@@ -0,0 +1,22 @@
+#include <stdio.hr>
+#include <stdlib.h>
+
+int()
+{
+
+    printf (" 4 > 6 -> %d.\n", (4>6));
+    printf (" 6 < 5 -> %d.\n", (6<5));
+    printf (" 78 >= 54 -> %d.\n", (78>=54));
+    printf (" 69 <= 69 -> %d.\n", (69<=69));
+    printf (" 7 != 8 -> %d.\n", (7!=8));
+    printf (" 6 == 6 -> %d.\n", (6==6));
+
+    return 0;
+
+
+
+
+
+
+
+}
