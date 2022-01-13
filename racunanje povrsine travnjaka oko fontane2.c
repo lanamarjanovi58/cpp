7c697681fb7016f3450fb777bc9c585df0426d8d
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-01-13 09:57:47 +0100

    Add files via upload

---
+++ racunanje povrsine travnjaka oko fontane2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main();
+{
+    float rf;
+    float rt;
+
+
+    printf("Polumjer fontane:\n");
+    scanf ("%f", %rf);
+    printf("Polumjer travnjaka:\n");
+    scanf("%f", &rt);
+    float PovrsinaFontane = rf*rf*3.14159;
+    float PovrsinaTravnjaka = rt*rt*3.14159
+    printf ("Povrsina travnjaka %.of",povrsinaTravnjaka-povrsinaFontane);
+
+    return 0;
+
+
+
+
+
+}
