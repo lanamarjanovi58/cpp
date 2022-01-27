bf90e5554f493aa3c634b08d8767ee4176e915ff
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-01-27 10:36:10 +0100

    Add files via upload

---
+++ stoljece4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a;
+   printf("unesi neku godinu\n");
+   scanf("%d",&a);
+   int x=((a-1)/100)+1;
+   printf("to je \n%d stoljece",x);
+}
