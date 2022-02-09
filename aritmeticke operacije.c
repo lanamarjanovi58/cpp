091044b154f4b242114b3b6c596587ad593442f8
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-02-09 19:00:53 +0100

    Add files via upload

---
+++ aritmeticke operacije.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y;
+    x=7;
+    y=9;
+
+    printf("%d + %d\n", x,y);
+    printf("%d - %d\n", x,y);
+    printf("%d * %d\n", x,y);
+    printf("%d / %d\n", x,y);
+    return 0;
+}
