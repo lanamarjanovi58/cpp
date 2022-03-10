203f0690beeb2616981332a36059eef23faf86c8
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-03-10 07:30:24 +0100

    Add files via upload

---
+++ paran ili neparan.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf ("Unesite cijeli broj\n");
+    scanf ("%d", &a);
+
+    if (a % 2 != 0);
+    {
+
+    printf("Broj je neparan");
+   {
+
+    if(a % 2 == 0)
+        printf("broj je paran");
+
+
+
+
+    return 0;
+
+
+
+}
