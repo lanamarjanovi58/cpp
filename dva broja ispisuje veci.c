de2f2cd3b0d004396c4cadeddbc47ad2f2642f51
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-03-10 07:50:05 +0100

    Add files via upload

---
+++ dva broja ispisuje veci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+
+printf("Unesi jedan broj:\n");
+scanf ("%d", &a);
+printf("Unesi drugi broj;\n");
+scanf("%d", &b);
+
+if(a>b)
+printf("Prvi broj (%d) je veæi od drugog broja (%d)",a,b);
+else
+printf ("drugi broj (%d) je veæi od drugog broja (%d)",b,a);
+
+  return 0;
+
+
+
+
+
+
+
+}
