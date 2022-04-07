e8b6fcf8cd68ddf51446689298463c8f53ec67a8
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-04-07 08:03:15 +0200

    Add files via upload

---
+++ formula.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+   int n;
+   float a,b,c,d=1;
+   printf("Unesi brojeve sa decimalom\n");
+
+
+   while(d){
+    a=b;
+    b=c;
+    c=d;
+    scanf("%f", &d);
+
+   }
+   printf("razlika %2,f", a-c);
+
+}
