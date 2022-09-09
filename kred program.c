85c452690602cb4649734e2a09b3f34c1a8aac53
lanamarjanovi58 <95843228+lanamarjanovi58@users.noreply.github.com>
2022-09-09 09:08:20 +0200

    Add files via upload

---
+++ kred program.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+
+    printf("Unesi kreditnu cijenu: ");
+    scanf("%d", &x);
+
+    if(x<750){
+    printf ("Nemate pristup: ");
+
+
+    }else{
+
+    printf("Dobrodosli u KRED program :");
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
